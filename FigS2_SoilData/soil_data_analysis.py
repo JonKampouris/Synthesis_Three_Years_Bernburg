2357b9d670c9ff98a6f88177f693e7297efae061
Marie Raab <marie.raab@hs-anhalt.de>
2026-04-10 11:05:14 +0200

    Update 2

--- FigS2_SoilData/soil_data_analysis.py
+++ FigS2_SoilData/soil_data_analysis.py
@@ -32,6 +32,6 @@ fig.text(0.5, 0.91, '(Baseline: 2009; Analysis Period: 2019 — 2021)', ha='cent
 fig.text(0.5, 0.03, 'Experimental Year', ha='center', va='center', fontsize=13)
 
 plt.subplots_adjust(hspace=0.2, top=0.88, bottom=0.12, right=0.8)
-plt.savefig('SoilData2009.jpg', format='jpeg', dpi=300)
+plt.savefig('FigS2_SoilData.jpg', format='jpeg', dpi=300)
 plt.show()
 
